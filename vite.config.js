6d5243e71fbaf593711453de5e1b4f9bc057e9ba
yYy135Captain <yuxuan.sun@honeywellaerospace.us>
2026-07-22 12:23:51 -0700

    fix workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DU1080-Documentation-React/',
+  base: '/DU1080-Documentation-React.github.io/',
 })
