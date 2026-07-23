702e34126969e929c847a27f12f87849522f3762
yYy135Captain <yuxuan.sun@honeywellaerospace.us>
2026-07-23 16:20:03 -0700

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import './styles/App.css'
+import './styles/Header.css'
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false)
