ae705a3c5fc5ba08229dbf608993996d326d87ed
yYy135Captain <yuxuan.sun@honeywellaerospace.us>
2026-07-27 12:53:10 -0700

    navlink for instrument

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
-import {Navigate, NavLink, Route, Routes, } from "react-router-dom"
+import {Navigate, NavLink, Route, Routes, useLocation, } from "react-router-dom"
 
 import Header from './components/Header'
+import Home from "./pages/Home.jsx"
 
-import AGMTestBox from "./pages/Instrument/AGMTestBox.mdx";
-import DAQ from "./pages/Instrument/DAQ.mdx";
+import AGMTestBox from "./pages/Instrument/AGMTestBox.mdx"
+import DAQ from "./pages/Instrument/DAQ.mdx"
+import GraphicsDiscreteIO from "./pages/Instrument/GraphicsDiscreteIO.mdx"
+import MUX from "./pages/Instrument/MUX.mdx"
+import MxFoundation from "./pages/Instrument/MxFoundation.mdx"
+import PickeringRelay from "./pages/Instrument/PickeringRelay.mdx"
+import Relay from "./pages/Instrument/Relay.mdx"
 
 import './styles/App.css'
 import './styles/Header.css'
@@ -14,6 +20,8 @@ import "./styles/Content.css"
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false)
   const [searchValue, setSearchValue] = useState("")
+
+  const location = useLocation()
 
   const handleThemeToggle = () => {
     setIsDarkMode((currentMode) => !currentMode)
@@ -52,24 +60,40 @@ function App() {
 
           <h2>Table of Contents</h2>
 
+          <NavLink to="/" end className={({ isActive }) => isActive ? "active-page-link" : ""} > Welcome </NavLink>
+
+
           <div className="toc-section">Instruments</div>
 
-          <div className = "toc_items">
-            <NavLink to="/instrument/agm-test=box">AGMTestBox</NavLink>
-            <NavLink to="/instrument/chroma2238">Chroma2238</NavLink>
-            <NavLink to="/instrument/daq">DAQ</NavLink>
-            <NavLink to="/instrument/dmm">DMM</NavLink>
-            <NavLink to="/instrument/graphics-discrete-io">GraphicsDiscreteIO</NavLink>
-            <NavLink to="/instrument/interface-box">InterfaceBox</NavLink>
-            <NavLink to="/instrument/mux">MUX</NavLink>
-            <NavLink to="/instrument/mx-foundation">MxFoundation</NavLink>
-            <NavLink to="/instrument/photometer">Photometer</NavLink>
-            <NavLink to="/instrument/pickering-relay">PickeringRealy</NavLink>
-            <NavLink to="/instrument/power-supply">PowerSupply</NavLink>
-            <NavLink to="/instrument/relay">Relay</NavLink>
-            <NavLink to="/instrument/serial-uart">SerialUART</NavLink>
+          <div className="toc-items">
 
-            <div class = "slider-line"></div>
+            <NavLink to="/instrument/agm-test=box" className={({ isActive }) => isActive ? "active-page-link" : ""} >AGMTestBox</NavLink>
+
+            <NavLink to="/instrument/chroma2238" className={({ isActive }) => isActive ? "active-page-link" : ""} >Chroma2238</NavLink>
+
+            <NavLink to="/instrument/daq" className={({ isActive }) => isActive ? "active-page-link" : ""} >DAQ</NavLink>
+
+            <NavLink to="/instrument/dmm" className={({ isActive }) => isActive ? "active-page-link" : ""} >DMM</NavLink>
+
+            <NavLink to="/instrument/graphics-discrete-io" className={({ isActive }) => isActive ? "active-page-link" : ""} >GraphicsDiscreteIO</NavLink>
+
+            <NavLink to="/instrument/interface-box" className={({ isActive }) => isActive ? "active-page-link" : ""} >InterfaceBox</NavLink>
+
+            <NavLink to="/instrument/mux" className={({ isActive }) => isActive ? "active-page-link" : ""} >MUX</NavLink>
+
+            <NavLink to="/instrument/mx-foundation" className={({ isActive }) => isActive ? "active-page-link" : ""} >MxFoundation</NavLink>
+
+            <NavLink to="/instrument/photometer" className={({ isActive }) => isActive ? "active-page-link" : ""} >Photometer</NavLink>
+
+            <NavLink to="/instrument/pickering-relay" className={({ isActive }) => isActive ? "active-page-link" : ""} >PickeringRealy</NavLink>
+
+            <NavLink to="/instrument/power-supply" className={({ isActive }) => isActive ? "active-page-link" : ""} >PowerSupply</NavLink>
+
+            <NavLink to="/instrument/relay" className={({ isActive }) => isActive ? "active-page-link" : ""} >Relay</NavLink>
+
+            <NavLink to="/instrument/serial-uart" className={({ isActive }) => isActive ? "active-page-link" : ""} >SerialUART</NavLink>
+
+            <div className = "slider-line"></div>
           </div>
           
          </nav>
@@ -78,7 +102,7 @@ function App() {
 
         <main className="content">
           <Routes>
-            <Route path="/" element={<AGMTestBox />}  />
+            <Route path="/" element={<Home />}  />
 
             <Route path="/instrument/agm-test-box" element={<AGMTestBox />}  />
             <Route path="/instrument/daq" element={<DAQ />} />
@@ -87,19 +111,28 @@ function App() {
         </main>
 
         <aside className="right-sidebar">
-          <h2>Overview</h2>
-
-          <a href="#send-agm-packet"></a>
-          <a href="#c-syntax">C Syntax</a>
-          <a href="#purpose">Purpose</a>
-          <a href="#parameters">Parameters</a>
-          <a href="#return-values">Return Values</a>
+          {currentOutline.length > 0 && (
+            <>
+              <h2>Overview</h2>
+              <nav className="page-outline"
+              aria-label="On this page"
+              >
+              {currentOutline.map((item) => (
+                <a 
+                  key={item.id}
+                  href={`#${item.id}`}
+                >
+                  {item.label}
+                </a>
+              ))}
+            </nav>
+           </> 
+          )}
+       
         </aside>
         </div>
       </div>
   )
 }
-  
-
 
 export default App 
