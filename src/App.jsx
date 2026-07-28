b8b7c89a274456af179dfbe21bf8efc0b285df95
yYy135Captain <yuxuan.sun@honeywellaerospace.us>
2026-07-28 10:26:39 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useRef, useState } from 'react' 
+import {useEffect, useMemo, useState } from 'react' 
 import {NavLink, Route, Routes, useLocation, } from "react-router-dom" 
 
 
